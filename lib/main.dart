dac43695e06efc3ec918e9d02f855181de9e6aec
Tosin2289 <ayoolatosin00@gmail.com>
2023-02-12 00:34:03 +0100

    third commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,384 @@
-import 'package:flutter/material.dart';
+import "package:flutter/material.dart";
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: ukit4(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class ukit4 extends StatefulWidget {
+  const ukit4({Key? key}) : super(key: key);
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  State<ukit4> createState() => _ukit4State();
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _ukit4State extends State<ukit4> {
+  TextEditingController firstname = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    var size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: SafeArea(
+          child: Container(
+        padding: EdgeInsets.only(
+            left: size.width / 15,
+            top: size.width / 15,
+            bottom: size.width / 15),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          crossAxisAlignment: CrossAxisAlignment.start,
+          // mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Row(
+              children: [
+                Container(
+                  height: size.width / 8,
+                  width: size.width / 8,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(size.width),
+                      image: const DecorationImage(
+                          fit: BoxFit.fill,
+                          image: AssetImage("assets/girl.jpg"))),
+                ),
+                SizedBox(
+                  width: size.width / 25,
+                ),
+                Column(
+                  children: [
+                    Text(
+                      "master mind",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: size.width / 23),
+                    ),
+                    Row(
+                      children: const [
+                        Text("meditate"),
+                        Text(
+                          " - 21h",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+                SizedBox(
+                  width: size.width / 2.8,
+                ),
+                Container(
+                  height: size.width / 8,
+                  width: size.width / 8,
+                  child: const Icon(Icons.widgets_outlined),
+                  decoration: BoxDecoration(
+                      color: Colors.grey[200],
+                      borderRadius: BorderRadius.circular(size.width)),
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            SizedBox(
+              height: size.width / 12,
             ),
+            Row(
+              children: [
+                Text(
+                  "Today/s reflexion",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: size.width / 220,
+                      color: Colors.indigo[400]),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: size.width / 24,
+            ),
+            Container(
+              padding: EdgeInsets.only(right: size.width / 5),
+              child: Text(
+                "Look deep into nature, and then you will understand everything better.",
+                style: TextStyle(
+                    fontSize: size.width / 25,
+                    wordSpacing: size.width / 180,
+                    letterSpacing: size.width / 240,
+                    fontStyle: FontStyle.italic),
+              ),
+            ),
+            SizedBox(
+              height: size.width / 27,
+            ),
+            Container(
+              alignment: Alignment.topRight,
+              padding: EdgeInsets.only(right: size.width / 10),
+              child: Text(
+                "-- Albert Enisten",
+                style: TextStyle(
+                    color: Colors.indigo[400], fontStyle: FontStyle.italic),
+              ),
+            ),
+            SizedBox(
+              height: size.width / 11,
+            ),
+            Container(
+              //alignment: Alignment.topLeft,
+              padding: EdgeInsets.only(
+                  left: size.width / 15, right: size.width / 15),
+              height: size.width / 8.5,
+              width: size.width / 1.2,
+              decoration: BoxDecoration(
+                  color: Colors.blue[50],
+                  borderRadius: BorderRadius.circular(size.width)),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: const [
+                  Text("time for meditation?"),
+                  Icon(Icons.search)
+                ],
+              ),
+            ),
+            SizedBox(
+              height: size.width / 11,
+            ),
+            Row(
+              //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Icon(
+                      Icons.favorite,
+                      color: Colors.indigo[400],
+                      size: size.width / 22,
+                    ),
+                    const Text(
+                      "Collections",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  width: size.width / 3,
+                ),
+                Row(
+                  //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text("Explore all"),
+                    Icon(
+                      Icons.arrow_forward_ios,
+                      size: size.width / 25,
+                    ),
+                  ],
+                )
+              ],
+            ),
+            SizedBox(
+              height: size.width / 18,
+            ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  Column(
+                    children: [
+                      Stack(
+                        children: [
+                          Container(
+                            height: size.width / 1.3,
+                            width: size.width / 3.3,
+                            decoration: BoxDecoration(
+                                color: Colors.black,
+                                borderRadius: BorderRadius.circular(size.width),
+                                image: const DecorationImage(
+                                    fit: BoxFit.fill,
+                                    image: AssetImage("assets/leaf.jpg"))),
+                          ),
+                          Positioned(
+                            bottom: size.width / 14,
+                            left: size.width / 8.9,
+                            child: Container(
+                              height: size.width / 7,
+                              width: size.width / 10,
+                              decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius:
+                                      BorderRadius.circular(size.width)),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Icon(
+                                    Icons.favorite,
+                                    color: Colors.indigo[400],
+                                  ),
+                                  Text(
+                                    "10k",
+                                    style: TextStyle(
+                                        fontSize: size.width / 40,
+                                        fontWeight: FontWeight.bold),
+                                  )
+                                ],
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                      const Text(
+                        "Nature",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      Row(
+                        children: [
+                          Text(
+                            "153",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: size.width / 30),
+                          ),
+                          Icon(
+                            Icons.play_circle,
+                            size: size.width / 25,
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    width: size.width / 17,
+                  ),
+                  Column(
+                    children: [
+                      Stack(
+                        children: [
+                          Container(
+                            height: size.width / 1.3,
+                            width: size.width / 3.3,
+                            decoration: BoxDecoration(
+                                color: Colors.black,
+                                borderRadius: BorderRadius.circular(size.width),
+                                image: const DecorationImage(
+                                    fit: BoxFit.fill,
+                                    image: AssetImage("assets/waterfall.jpg"))),
+                          ),
+                          Positioned(
+                            bottom: size.width / 14,
+                            left: size.width / 8.9,
+                            child: Container(
+                              height: size.width / 7,
+                              width: size.width / 10,
+                              decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius:
+                                      BorderRadius.circular(size.width)),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Icon(
+                                    Icons.favorite_border,
+                                    color: Colors.indigo[400],
+                                  ),
+                                  Text(
+                                    "852",
+                                    style: TextStyle(
+                                        fontSize: size.width / 40,
+                                        fontWeight: FontWeight.bold),
+                                  )
+                                ],
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                      const Text(
+                        "Birds",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      Row(
+                        children: [
+                          Text(
+                            "324",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: size.width / 30),
+                          ),
+                          Icon(
+                            Icons.play_circle,
+                            size: size.width / 25,
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    width: size.width / 17,
+                  ),
+                  Column(
+                    children: [
+                      Stack(
+                        children: [
+                          Container(
+                            height: size.width / 1.3,
+                            width: size.width / 3.3,
+                            decoration: BoxDecoration(
+                                color: Colors.black,
+                                borderRadius: BorderRadius.circular(size.width),
+                                image: const DecorationImage(
+                                    fit: BoxFit.fill,
+                                    image: AssetImage("assets/bird.jpg"))),
+                          ),
+                          Positioned(
+                            bottom: size.width / 14,
+                            left: size.width / 8.5,
+                            child: Container(
+                              height: size.width / 7,
+                              width: size.width / 10,
+                              decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius:
+                                      BorderRadius.circular(size.width)),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Icon(
+                                    Icons.favorite,
+                                    color: Colors.indigo[400],
+                                  ),
+                                  Text(
+                                    '1k',
+                                    style: TextStyle(
+                                        fontSize: size.width / 40,
+                                        fontWeight: FontWeight.bold),
+                                  )
+                                ],
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                      const Text(
+                        "Waterfall",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      Row(
+                        children: [
+                          Text(
+                            "601",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: size.width / 30),
+                          ),
+                          Icon(
+                            Icons.play_circle,
+                            size: size.width / 25,
+                          )
+                        ],
+                      )
+                    ],
+                  )
+                ],
+              ),
+            )
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      )),
     );
   }
 }
